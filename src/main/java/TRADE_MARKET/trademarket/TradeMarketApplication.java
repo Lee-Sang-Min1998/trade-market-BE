d3499b29c4f6ae0be247a242dbf1d2a6f75bf735
Lee sang min <dkwkghkdlxld2@gmail.com>
2023-08-08 16:30:10 +0900

    init project

---
+++ src/main/java/TRADE_MARKET/trademarket/TradeMarketApplication.java
@@ -0,0 +1,13 @@
+package TRADE_MARKET.trademarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TRADE_MARKET/trademarket/TradeMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package TRADE_MARKET.trademarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
