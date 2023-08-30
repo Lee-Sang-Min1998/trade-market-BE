a0d81ac0b6a9b3d866086a23bb111d253c6edf50
leesangmin <dkwkghkdlxld2@gmail.com>
2023-08-30 22:00:10 +0900

    feat: spring security config 생성

---
+++ src/main/java/TRADE_MARKET/trademarket/global/config/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package TRADE_MARKET.trademarket.global.config;
+
+import TRADE_MARKET.trademarket.global.auth.domain.JwtAccessDeniedHandler;
+import TRADE_MARKET.trademarket.global.auth.domain.JwtAuthenticationEntryPoint;
+import TRADE_MARKET.trademarket.global.auth.service.JwtTokenProvider;
+import TRADE_MARKET.trademarket.global.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .cors(cors -> cors.disable())
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement((sessionManagement) ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+            .authorizeHttpRequests((authorizationRequests) ->
+                authorizationRequests.requestMatchers("/api/login/**").permitAll()
+                    .anyRequest().authenticated());
+
+        http
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling(e ->
+                e.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                    .accessDeniedHandler(jwtAccessDeniedHandler));
+
+        return http.build();
+    }
+
+}
