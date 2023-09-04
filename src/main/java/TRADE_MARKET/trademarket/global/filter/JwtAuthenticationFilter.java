dd6f31a8074e34e089e2b77b3e4a8ddce1cabecb
leesangmin <dkwkghkdlxld2@gmail.com>
2023-09-04 22:20:11 +0900

    fix: JwtAuthenticationFilter이 Security Filter에 속하지 않는 에러 해결

--- src/main/java/TRADE_MARKET/trademarket/global/filter/JwtAuthenticationFilter.java
+++ src/main/java/TRADE_MARKET/trademarket/global/filter/JwtAuthenticationFilter.java
@@ -13,18 +13,23 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
-@Component
-@RequiredArgsConstructor
+
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
     private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
